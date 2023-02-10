6edefc42c92cd91690090be7b5d6745202a2b221
Artur Pushkov <arturpushkov@MacBook-Pro-Artur.local>
2023-02-10 12:39:32 +0200

    Renamed root component shop-app.tsx into App.tsx in order to follow react.js components naming best practises

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Modal from "react-modal"
 import "./index.css";
-import { ShopApp } from "./shop-app";
+import {App} from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 Modal.setAppElement("#root")
 
 ReactDOM.render(
   <React.StrictMode>
-    <ShopApp />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
